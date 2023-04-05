5f707a126a34134d2d0086845ed72e7ded088ee5
Sahil Sharma <vlsharma713@gmail.com>
2023-04-05 19:30:25 +0530

    Add base template

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("index.html")
+
+
+@app.route("/fashion")
+def fashion():
+    return render_template("fashion.html")
+
+
+@app.route("/electronic")
+def electronic():
+    return render_template("electronic.html")
+
+
+@app.route("/jewellery")
+def jewellery():
+    return render_template("jewellery.html")
 
 
 if __name__ == '__main__':
