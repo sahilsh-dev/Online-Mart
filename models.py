bc2381169865848db930dd7d6b69e05afc271738
Sahil Sharma <vlsharma713@gmail.com>
2023-11-21 21:55:51 +0530

    Add models

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+
+class Product(db.Model):
+    __tablename__ = "products" 
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer, nullable=False)
+    stock_quantity = db.Column(db.Integer, nullable=False)
+    images = db.relationship('ProductImages', backref='product', lazy=True) 
+    
+
+class ProductImages(db.Model):
+    __tablename__ = "product_images"
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    image_name = db.Column(db.String(50), nullable=False) 
+    
