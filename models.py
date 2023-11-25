9a026433c7ed7da786ea5a855bb4b8a18b792bb6
Sahil Sharma <vlsharma713@gmail.com>
2023-11-25 20:22:48 +0530

    Add categories model

--- models.py
+++ models.py
@@ -2,6 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+product_category = db.Table('product_category',
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id')),
+    db.Column('category_id', db.Integer, db.ForeignKey('categories.id'))
+)
+
+
 class Product(db.Model):
     __tablename__ = "products" 
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +16,7 @@ class Product(db.Model):
     price = db.Column(db.Integer, nullable=False)
     stock_quantity = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
+    in_categories = db.relationship('Category', secondary=product_category, backref='products')
     images = db.relationship('ProductImage', backref='product', lazy=True) 
     
     def __repr__(self):
@@ -30,7 +37,7 @@ class Collection(db.Model):
     __tablename__ = "collections"
     collection_name = db.Column(db.String(100), primary_key=True)
     products = db.relationship('CollectionItem', backref='collection', lazy=True)
-    
+     
     def __repr__(self):
         return f'<Collection {self.collection_name}>'
     
@@ -40,3 +47,12 @@ class CollectionItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     collection_name = db.Column(db.String(100), db.ForeignKey('collections.collection_name'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(100), primary_key=True)
+    
+    def __repr__(self):
+        return f'<Category {self.category_name}>' 
